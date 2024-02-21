fcf32f9b0b53c1199df7b2a20d0905ccb919afb0
Mau_2993 <shastrimayur2993@gmail.com>
2024-02-21 09:11:53 +0530

    shadcn configured

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button onClick={()=>{
+        alert("ShadCn is the best!");
+      }}>Submit</Button>
+    </>
+  );
 }
