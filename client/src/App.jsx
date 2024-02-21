8cabaa5619bfdda26e11a4b2344ad01d4637ea41
Sagnik Majumder <sagnikm183@gmail.com>
2024-02-21 18:55:04 +0530

    basic skeleton code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
-import { Button } from "@/components/ui/button"
-
+import About from "./components/devpages/About";
+import Contact from "./components/devpages/Contact";
+import LandingPage from "./components/landingpage/landingpage";
+import { Navbar } from "./components/navbar/header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 export default function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <Button onClick={()=>{
-        alert("ShadCn is the best!");
-      }}>Submit</Button>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
