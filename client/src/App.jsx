27f566dc864a734a0e54c0427c3ea36cc004bc6c
Mau_2993 <shastrimayur2993@gmail.com>
2024-02-21 15:53:21 +0530

    CORS setup complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 import { Button } from "@/components/ui/button"
+import {instance as axios} from "../axiosConfig";
+import { useEffect, useState } from "react";
 
 export default function App() {
+
+  const [welcome, setWelcome] = useState("Loading...");
+
+  useEffect(()=>{
+    const fetchWelcomeMessage = async ()=>{
+      const res = await axios.get('/');
+      console.log(res.data);
+      setWelcome(res.data.message);
+    }
+    fetchWelcomeMessage();
+  }, []);
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        {welcome}
       </h1>
       <Button onClick={()=>{
         alert("ShadCn is the best!");
--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+}
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send({message: 'Hello World!!'});
 });
 
 const port = process.env.PORT || 3000
