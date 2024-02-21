699b43bf2614fa6a3c948e38a202db7cbe5ea10f
Mau_2993 <shastrimayur2993@gmail.com>
2024-02-21 19:25:07 +0530

    Basic backend setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const httpServer = require('http').createServer(app);
+const io = require('socket-io')(httpServer);
+
+const port = process.env.PORT || 3000
+httpServer.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 corsOptions = {
     origin: 'http://localhost:5173',
@@ -12,9 +19,4 @@ app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.send({message: 'Hello World!!'});
-});
-
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
 });
