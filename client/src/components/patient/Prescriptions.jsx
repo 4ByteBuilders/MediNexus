3badae6335aee9d7e13b975f4f92709cb2a1e358
Sagnik Majumder <sagnikm183@gmail.com>
2024-03-20 19:00:32 +0530

    modified patient dashboard

--- client/src/components/patient/Prescriptions.jsx
+++ client/src/components/patient/Prescriptions.jsx
@@ -1,9 +1,11 @@
 import Sidebar from "../navbar/sideBar"
-
+import { instance as axios } from "@/lib/axiosConfig";
 import { IoNewspaper } from "react-icons/io5";
 import { GrDocumentTest } from "react-icons/gr";
 import { MdDashboard } from "react-icons/md";
 import { IoMdChatbubbles } from "react-icons/io";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 const sideBarItemsUpper = [
     {
         name: "Dashboard",
@@ -27,17 +29,38 @@ const sideBarItemsUpper = [
     },
 ];
 
-function Prescriptions() {
+function Prescriptions({ patientData }) {
+    console.log(patientData)
+    const [prescriptions, setPrescriptions] = useState([])
+    useEffect(() => {
+        const fetchPrescriptions = async () => {
+            try {
+                const res = await axios.get('/prescription/:patient_id/get-prescription')
+                setPrescriptions(res.data)
+                toast.success('Prescriptions fetched successfully')
+            } catch (error) {
+                console.log(error)
+                toast.error('Error fetching prescriptions. Please try again later.')
+            }
+        }
+        fetchPrescriptions()
+    }, [])
     return (
-        <div className="w-screen pl-[220px] pt-8 font-lato">
-            <Sidebar items={sideBarItemsUpper} />
-            <div className="w-full flex flex-col">
-                <h1 className="text-2xl font-bold">Your Prescriptions</h1>
-                <div className="mr-5 mt-8 flex flex-row gap-5" >
-
-                </div>
-                <div>
-                </div>
+        <div className="w-full flex flex-col">
+            <h1 className="text-2xl font-bold">Your Prescriptions</h1>
+            <div className="mr-5 mt-8 flex flex-row gap-5">
+                {
+                    prescriptions.map((prescription, idx) => {
+                        return (
+                            <div key={idx}>
+                                <h2 className="font-semibold">Prescription {idx + 1}</h2>
+                                {/* <p>{prescription}</p> */}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div>
             </div>
         </div>
     )
