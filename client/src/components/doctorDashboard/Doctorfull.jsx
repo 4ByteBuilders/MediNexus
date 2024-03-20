95af710065ac1d0c15c9ef39b1b4ca7834291014
BHAVYAJAIX <bhavyajain16104@gmail.com>
2024-03-20 21:22:03 +0530

    prescriptionform

--- client/src/components/doctorDashboard/Doctorfull.jsx
+++ client/src/components/doctorDashboard/Doctorfull.jsx
@@ -4,7 +4,9 @@ import { LiaBlogSolid } from "react-icons/lia";
 import { IoMdChatbubbles } from "react-icons/io";
 import { IoIosNotifications } from "react-icons/io";
 import { Button } from "../ui/button";
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import PrescriptionForm from "./PrescriptionForm";
 const sideBarItemsUpper = [
   {
     name: "Prescriptions",
@@ -28,6 +30,20 @@ const sideBarItemsUpper = [
   },
 ];
 const Doctorfull = () => {
+  const [prescription, setPrescriptions] = useState([
+    {
+      patientname: "Abhishek Ekre",
+      doctorName: "Dr. Shastru",
+      hospitalname: "Apollo Hospital",
+      created: "12/5/22",
+    },
+    {
+      patientname: "Abhishek Ekre",
+      doctorName: "Dr. Shastru",
+      hospitalname: "Apollo Hospital",
+      created: "12/5/22",
+    },
+  ]);
   return (
     <>
       <div className="w-screen pl-[220px] pt-8 font-lato">
@@ -39,33 +55,24 @@ const Doctorfull = () => {
           <div className="my-8">
             <h1 className="text-2xl font-bold">Patient's Prescriptions</h1>
           </div>
-          <div className="w-1/2 p-6 bg-slate-200 rounded-lg shadow-sm my-3">
-            <h2 className="text-2xl font-semibold text-gray-800 mb-2">
-              Abhishek Ekre
-            </h2>
-            <p className="text-sm text-gray-500 mb-2">Created: 12/5/22</p>
-            <p className="text-lg text-gray-700 mb-2">Disease: Piles</p>
-            <p className="text-base text-gray-600">
-              Opinion: We of Alcoholics Anonymous believe that the reader will
-              be interested in the medical estimate of the plan of recovery
-              described in this book.
-            </p>
-          </div>
-          <div className="w-1/2 p-6 bg-slate-200 rounded-lg shadow-sm my-3  ">
-            <h2 className="text-2xl font-semibold text-gray-800 mb-2">
-              Abhishek Ekre
-            </h2>
-            <p className="text-sm text-gray-500 mb-2">Created: 12/5/22</p>
-            <p className="text-lg text-gray-700 mb-2">Disease: Piles</p>
-            <p className="text-base text-gray-600">
-              Opinion: We of Alcoholics Anonymous believe that the reader will
-              be interested in the medical estimate of the plan of recovery
-              described in this book.
-            </p>
-          </div>
-          <div className="flex justify-end my-8 mr-20">
-            <Button>Create new Prescription</Button>
-          </div>
+          {prescription.map((p, id) => {
+            return (
+              <div
+                key={id}
+                className="w-1/2 p-6 bg-slate-200 rounded-lg shadow-sm my-3  "
+              >
+                <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+                  {p.patientname}
+                </h2>
+                <p className="text-sm text-gray-500 mb-2">
+                  Created: {p.created}
+                </p>
+                <div className="flex justify-end">
+                  <Link to="/prescriptionform">Fill Prescription</Link>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
--- client/src/components/doctorDashboard/PrescriptionForm.jsx
+++ client/src/components/doctorDashboard/PrescriptionForm.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   Form,
   FormControl,
@@ -8,19 +7,155 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { Button } from "../ui/button";
 
-const PrescriptionForm = () => {
+const prescriptionSchema = z.object({
+  symptoms: z.array(z.string()),
+  medicines: z.array(z.string()),
+  doctorOpinion: z.string().min(2).max(50),
+  disease: z.string().min(4).max(20),
+  recommendedTests: z.string().min(0).max(50),
+});
+
+const initialValuesPatient = {
+  symptoms: "",
+  medicines: "",
+  doctorOpinion: "",
+  disese: "",
+  recommendedTests: "",
+};
+
+const PrescriptionForm = ({ doctor, created, patientname, hospitalname }) => {
+  const form = useForm({
+    resolver: zodResolver(prescriptionSchema),
+    defaultValues: initialValuesPatient,
+  });
+
+  async function onSubmit(values) {
+    console.log(values);
+    try {
+      const res = await axios.post("/patient-auth/login", values);
+      console.log(res);
+      toast.success("Hospital logged in successfully");
+      setTimeout(() => {
+        window.location.href = "/hospitalhome";
+      }, 1500);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    }
+  }
+
   return (
-    <>
-      <Form>
-        <FormItem>
-          <FormLabel className="text-md">Patient Name</FormLabel>
-          <FormControl>
-            <Input placeholder="John Doe" className="text-md" />
-          </FormControl>
-        </FormItem>
+    <div className="m-8 mx-96 w-1/2 p-6 bg-slate-200 rounded-lg shadow-sm my-3">
+      <div>
+        <h1 className="text-2xl font-bold">Fill Prescription</h1>
+      </div>
+      <div>
+        <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+          Patient: {patientname}
+        </h2>
+        <p className="text-sm text-gray-500 mb-2">Doctor: {doctor}</p>
+        <p className="text-sm text-gray-500 mb-2">Hospital: {hospitalname}</p>
+        <p className="text-sm text-gray-500 mb-2">Created: {created}</p>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="symptoms"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-md">Symptoms</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Fever, Headache..."
+                    {...field}
+                    className="text-md"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="disease"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-md">Probable Disease</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Cholera, Typhod.."
+                    {...field}
+                    className="text-md"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="doctorOpinion"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-md">Opinion</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Doctor's remarks/opinion"
+                    {...field}
+                    className="text-md"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="medicines"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-md">Medicines</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Dolo, Crocin..."
+                    {...field}
+                    className="text-md"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="recommendedTests"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-md">Recommended tests</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="CT scan, MRI..."
+                    {...field}
+                    className="text-md"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
       </Form>
-    </>
+    </div>
   );
 };
 
