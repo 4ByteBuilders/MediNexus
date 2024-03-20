eed354afed1d844ce0ffab82f8606706094b408f
Sagnik Majumder <sagnikm183@gmail.com>
2024-03-20 10:38:16 +0530

    changed login signup

--- client/src/components/authentication/SwitchComponent.jsx
+++ client/src/components/authentication/SwitchComponent.jsx
@@ -1,26 +1,26 @@
-const items = [
-    {
-        title: "Hospital",
-    },
-    {
-        title: "Doctor",
-    }
-]
+import PropTypes from 'prop-types';
 
-function SwitchComponent({ user, setUser }) {
-
+function SwitchComponent({ user, setUser, items }) {
+    console.log(items)
     return (
         <div className="grid grid-cols-2 place-items-center rounded-full h-fit bg-slate-200 p-2 my-2">
             {
                 items.map((item, index) =>
                 (
-                    <div key={index} className={`${user === item.title ? "bg-slate-500 text-white" : ""} p-1 px-4 rounded-full cursor-pointer transition-colors duration-300 ease-in-out`}
-                        onClick={() => setUser(item.title)}>
-                        <h1>{item.title}</h1>
+                    <div key={index} className={`${user === item ? "bg-slate-500 text-white" : ""} p-1 px-4 rounded-full cursor-pointer transition-colors duration-300 ease-in-out`}
+                        onClick={() => setUser(item)}>
+                        <h1>{item}</h1>
                     </div>))
             }
         </div>
     )
+}
+
+//prop Validation
+SwitchComponent.propTypes = {
+    user: PropTypes.string.isRequired,
+    setUser: PropTypes.func.isRequired,
+    items: PropTypes.array.isRequired
 }
 
 export default SwitchComponent
